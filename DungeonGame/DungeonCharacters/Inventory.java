9b4e20ff81913eb558243b4e0630b87da2535f8e
= <=>
2020-03-11 19:31:27 -0700

    added inventory class

---
+++ DungeonGame/DungeonCharacters/Inventory.java
@@ -0,0 +1,28 @@
+package DungeonCharacters;
+
+public class Inventory {
+	private int healingPotions=0;
+	private int visionPotions=0;
+	private Hero player;
+	
+	public Inventory(final Hero player) {
+		this.player = player;
+	}
+	
+	public void addHealingPotion() {healingPotions++;}
+	public void addVisionPotion() {visionPotions++;}
+	public void removeHealingPotion() {healingPotions--;}
+	public void removeVisionPotion() {visionPotions--;}
+	
+	public void useHealingPotion() {
+		int amount = calculateHealingAmount();
+		player.addHitPoints(amount);
+		healingPotions--;
+	}
+	
+	private int calculateHealingAmount() {
+		//set amount from 20 to 50
+		int amount =(int)(Math.random()*((15)-5))+5;
+		return amount;
+	}
+}
